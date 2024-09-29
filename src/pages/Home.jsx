ec0c7ac52685882dbbed17e5b93fecdb3ec3a769
Lucas <lucas.voriarocha@gmail.com>
2024-09-29 02:17:57 -0300

    Create loading state for visitor count and changing icon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
-import axios from 'axios';
+import axios from 'axios'
 
-import { Header } from '../components/Header';
-import { SubscribeButton } from '../components/SubscribeButton';
+import { Header } from '../components/Header'
+import { SubscribeButton } from '../components/SubscribeButton'
 
 import homeImg from '../assets/avatar.svg'
 
 import '../styles/home.scss'
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 
 export function Home() {
-    const [visitorCount, setVisitorCount] = useState(0);
+    const [visitorCount, setVisitorCount] = useState(0)
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         const fetchVisitorCount = async () => {
-        try {
-            const response = await axios.get(`${BACKEND_URL}/visitor/count`)
-            setVisitorCount(response.data.totalCount);
-        } catch (error) {
-            console.error('Error fetching visitor count', error);
-        }
+            try {
+                const response = await axios.get(`${BACKEND_URL}/visitor/count`)
+                setVisitorCount(response.data.totalCount)
+                setLoading(false)
+            } catch (error) {
+                console.error('Error fetching visitor count', error)
+                setLoading(false);
+            }
         }
 
         const incrementVisitorCount = async () => {
             try {
-              const response = await axios.post(`${BACKEND_URL}/visitor/count/increment`)
-              console.log(response.data.message); // Optional: log success message
+                await axios.post(`${BACKEND_URL}/visitor/count/increment`)
             } catch (error) {
-              console.error('Error incrementing visitor count', error);
-          
-              }  }
+                console.error('Error incrementing visitor count', error)
+            }  
+        }
 
-        fetchVisitorCount();
+        fetchVisitorCount()
         incrementVisitorCount()
-    }, []);
+    }, [])
 
     return (
         <>
@@ -52,9 +54,11 @@ export function Home() {
                     </p>
                     <div className='visitorContainer'>
                         <SubscribeButton />
-                        <div className='visitorCount'>
-                            {visitorCount} visitantes até o momento
-                        </div>
+                        {!loading && (
+                            <div className='visitorCount'>
+                                {visitorCount} visitantes até o momento
+                            </div>
+                        )}
                     </div>
                 </section>
 
